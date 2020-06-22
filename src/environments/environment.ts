5232beeca5ccaa619522b68c2e01b18113dfed03
Emmanuel-9 <oregadaniel181@gmail.com>
2020-06-22 08:32:30 +0300

    Add the userUrl and reposUrl.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  userUrl: 'https://api.github.com/users/Emmanuel-9',
+  reposUrl: 'https://api.github.com/users/Emmanuel-9/repos',
 };
 
 /*
