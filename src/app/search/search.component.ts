6bae026f9d624f26dd89e15bde61bf9ddcbc4869
Emmanuel-9 <oregadaniel181@gmail.com>
2020-06-22 08:35:30 +0300

    Add the functions SubmitUserName(),goToRepos().

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Users } from '../users';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  userName: Users
+  retrievedUser: Users
+  word: string= "Click to show Repos... "
+  constructor(private http: HttpClient, private router: Router) {
+    this.userName = new Users("", "",0, "", "", new Date())
+   }
+
+  submitUserName(){
+     console.log(this.userName['name'])
+     this.http.get<Users>(`https://api.github.com/users/${this.userName['name']}`).subscribe({
+      //  next(response) {console.log(response)}
+      next: user => this.retrievedUser = user
+     })
+   }
+   goToRepos(){     
+     this.router.navigate(['/repository'], {state: {name: this.userName['name']}})
+   }
+
+
+  ngOnInit(): void {
+    
+  }
+}
